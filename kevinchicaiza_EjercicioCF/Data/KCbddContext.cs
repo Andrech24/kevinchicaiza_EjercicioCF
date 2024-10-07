dd6c207bc817359fa462680802eaa9e2b1da73c2
laboratorio <laboratorio@UPEAULA-31712>
2024-10-07 11:18:13 -0500

    Agregar archivos de proyecto.

---
+++ kevinchicaiza_EjercicioCF/Data/KCbddContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using kevinchicaiza_EjercicioCF.Models;
+
+    public class KCbddContext : DbContext
+    {
+        public KCbddContext (DbContextOptions<KCbddContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<kevinchicaiza_EjercicioCF.Models.Burger> Burger { get; set; } = default!;
+    }
